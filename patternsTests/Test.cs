b75ea47d6ade6b3497e2185e1f97e49355b3cf44
loktionov_a <loktionov.a@orionnet.ru>
2017-12-05 12:36:32 +0700

    NUnitTests

---
+++ patternsTests/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+namespace patternsTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Assert.AreEqual(1, 1, "1 should be a 1");
+        }
+
+        [Test()]
+        public void TestCase2()
+        {
+            Assert.AreEqual(4, 1, "4 shouldn't be a 1");
+        }
+    }
+}
