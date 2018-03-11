3af958d8abba8837f632b7f27bcd3199adaf2ef7
Alex Ander <loktionov129@gmail.com>
2018-03-11 13:49:39 +0700

    Add files via upload

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testAsp.Controllers
+{
+    using testAsp.Models;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Y0ur @pplication description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return View(new Class1());
+        }
+
+        [HttpPost]
+        public ActionResult Test(Class1 data)
+        {
+            return ModelState.IsValid ? View("Success", data) : View();
+        }
+    }
+}
---
+++ MVC/Models/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testAsp.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Class1
+    {
+        [Required(ErrorMessage = "Id is req")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "col1 is req")]
+        [MinLength(3, ErrorMessage = "Col1 is min 3 length")]
+        [MaxLength(3, ErrorMessage = "Col1 is max 5 length")]
+        public string Col1 { get; set; }
+    }
+}
