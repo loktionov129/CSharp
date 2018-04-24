f262bc9c20f1d0b27b17a5e5fa6a5a799a3f9278
loktionov129 <loktionov129@gmail.com>
2018-04-25 01:53:59 +0700

    Add NLog

---
+++ NLog/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NetRore.Csharp.Cmd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Launch();
+        }
+
+        private void Launch()
+        {
+            StaticLogger.LogInfo(GetType(), "Begin Launch...");
+            try
+            {
+                Console.WriteLine(42 / int.Parse("0"));
+            }
+            catch (Exception ex)
+            {
+                StaticLogger.LogException(GetType(), "Something went wrong", ex);
+            }
+            StaticLogger.LogInfo(GetType(), "End Launch...");
+        }
+    }
+}
---
+++ NLog/StaticLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NLog;
+
+namespace NetRore.Csharp.Cmd
+{
+    public static class StaticLogger
+    {
+        public static void LogException(Type type, string message, Exception exception)
+        {
+            LogManager.GetLogger(type.FullName).Error(exception);
+        }
+
+        public static void LogInfo(Type type, string message)
+        {
+            LogManager.GetLogger(type.FullName).Info(message);
+        }
+    }
+}
