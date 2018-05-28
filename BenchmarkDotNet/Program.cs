0c1c343e40991329c54302be88afa4c3e4448a41
Alexander Loktionov <alexander.loktionov@saritasa.com>
2018-05-28 13:28:13 +0700

    Add BenchmarkDotNet

---
+++ BenchmarkDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<StringBuilderTest>();
+        }
+    }
+}
---
+++ BenchmarkDotNet/StringBuilderTest.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class StringBuilderTest
+    {
+        const int N = 1000;
+        string[] strs = null;
+
+        [Setup]
+        public void Init()
+        {
+            strs = new string[] { "A", "Hello", "You", "B", "The", ".NET", "Opt" };
+        }
+
+        [Benchmark(Baseline = true)]
+        public string WithStringBuilder()
+        {
+            StringBuilder sb = new StringBuilder();
+            var len = strs.Length;
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append(strs[i % len]);
+            }
+            return sb.ToString();
+        }
+
+        [Benchmark]
+        public string WithoutStringBuilder()
+        {
+            string sb = string.Empty;
+            var len = strs.Length;
+            for (int i = 0; i < N; i++)
+            {
+                sb += strs[i % len];
+            }
+            return sb.ToString();
+        }
+    }
+}
